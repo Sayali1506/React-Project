195917ace2b5cc43150b342c5ae0826a0a1f39a0
Sayali.Chitale <Sayali.Chitale@e-zest.in>
2024-01-08 11:17:48 +0530

    Navigation bar created

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,28 @@
-import Nav from "./Nav";
-
 export default function Header() {
   return (
     <>
-      <div>
-        <img src="icons_assets/Logo.svg" alt="Little Lemon Restro" />
+      {/* Header */}
+      <div className="container">
+        <div className="grid-container">
+          <header>
+            <div className="logo">
+              <img src="icons_assets/Logo.svg" alt="Little Lemon Restro" />
+            </div>
+          </header>
+
+          {/* Navigation Bar */}
+          <nav>
+            <ul className="flex-container">
+              <li>Home</li>
+              <li>About</li>
+              <li>Menu</li>
+              <li>Reservation</li>
+              <li>Order Online</li>
+              <li>Login</li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      {/* Navigation component  */}
-      <Nav />
     </>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,3 @@
 export default function Nav() {
-  return (
-    <>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Menu</li>
-        <li>Reservation</li>
-        <li>Order Online</li>
-        <li>Login</li>
-      </ul>
-    </>
-  );
+  return <></>;
 }
