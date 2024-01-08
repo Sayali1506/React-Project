66ecf46e71dd153d9dca59af88936f5801adb3f9
Sayali.Chitale <Sayali.Chitale@e-zest.in>
2024-01-08 12:49:47 +0530

    final files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import AboutUs from "./components/AboutUs";
-import Hero from "./components/Hero";
-import BookingPage from "./components/BookingPage";
-import HomePage from "./components/HomePage";
-import Testimonials from "./components/Testimonials";
-import Specials from "./components/Specials";
+import React from 'react';
+import './App.css'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Main from './components/Main';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <>
-      {/* router */}
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/contact" element={<BookingPage />} />
-            <Route path="/specials" element={<Specials />} />
-            <Route path="/BookingPage" element={<BookingPage />} />
-            <Route path="/" element={<BookingPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </div>
-
-        {/* other components */}
-        <Hero />
-        <HomePage />
-        <Specials />
-        <Testimonials />
-        <AboutUs />
-        <BookingPage />
-      </Router>
+    <Nav />
+    <Main />
+    <Menu />
+    <Footer />
     </>
-  );
-}
+  )
+  }
 
 export default App;
