89e4463a92d1e8b683853d0825c7125f3d698047
Sayali.Chitale <Sayali.Chitale@e-zest.in>
2024-01-08 10:50:31 +0530

    Inital Markup-setup

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <>
+      <div>
+        <img src="icons_assets/Logo.svg" alt="Little Lemon Restro" />
+      </div>
+      {/* Navigation component  */}
+      <Nav />
+    </>
+  );
+}
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservation</li>
+        <li>Order Online</li>
+        <li>Login</li>
+      </ul>
+    </>
+  );
+}
